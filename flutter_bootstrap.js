79e6a4bc698bba0dd041fda7966e4859ad2b999e
hungmt <hungthe50@gmail.com>
2026-02-05 13:58:56 +0700

    cau hinh onsignal

--- flutter_bootstrap.js
+++ flutter_bootstrap.js
@@ -11,6 +11,6 @@ _flutter.buildConfig = {"engineRevision":"db49896cf25ceabc44096d5f088d86414e05a7
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "385650778"
+    serviceWorkerVersion: "3021644975"
   }
 });
